6b14413653a287fa0a5ec583fa8d0bb7d73e362e
Dlan18 <fkamil502@gmail.com>
2023-10-22 09:46:47 +0700

    first commit

---
+++ assets.js
@@ -0,0 +1,94 @@
+// IMPORT ASSETS!
+
+// Piano
+
+let audioDo = new Audio();
+audioDo.src = 'AudioPiano/Do.mp3';
+
+let audioRe = new Audio();
+audioRe.src = 'AudioPiano/Re.mp3';
+
+let audioMi = new Audio();
+audioMi.src = 'AudioPiano/Mi.mp3';
+
+let audioFa = new Audio();
+audioFa.src = 'AudioPiano/Fa.mp3';
+
+let audioSol = new Audio();
+audioSol.src = 'AudioPiano/Sol.mp3';
+
+let audioLa = new Audio();
+audioLa.src = 'AudioPiano/La.mp3';
+
+let audioSi = new Audio();
+audioSi.src = 'AudioPiano/Si.mp3';
+
+let audioDo2 = new Audio();
+audioDo2.src = 'AudioPiano/Do2.mp3';
+
+let audioP_C = new Audio();
+audioP_C.src = 'AudioPiano/P_C.mp3';
+
+let audioP_D = new Audio();
+audioP_D.src = 'AudioPiano/P_D.mp3';
+
+let audioP_F = new Audio();
+audioP_F.src = 'AudioPiano/P_F.mp3';
+
+let audioP_G = new Audio();
+audioP_G.src = 'AudioPiano/P_G.mp3';
+
+let audioP_A = new Audio();
+audioP_A.src = 'AudioPiano/P_A.mp3';
+
+// Gitar
+
+let audioG_C = new Audio();
+audioG_C.src = 'AudioGitar/C.mp3';
+
+let audioAM = new Audio();
+audioAM.src = 'AudioGitar/Am.mp3';
+
+let audioDM = new Audio();
+audioDM.src = 'AudioGitar/Dm.mp3';
+
+let audioG_G = new Audio();
+audioG_G.src = 'AudioGitar/G.mp3';
+
+let audioG_F = new Audio();
+audioG_F.src = 'AudioGitar/F.mp3';
+
+// Drum
+
+let audiohihatOpen = new Audio();
+audiohihatOpen.src = 'AudioDrum/hihat-open.mp3';
+
+let audiohihat = new Audio();
+audiohihat.src = 'AudioDrum/hihat.mp3';
+
+let audiocrash = new Audio();
+audiocrash.src = 'AudioDrum/crash.mp3';
+
+let audioshareDrum = new Audio();
+audioshareDrum.src = 'AudioDrum/share-drum.mp3';
+
+let audioshareStick = new Audio();
+audioshareStick.src = 'AudioDrum/share-stick.mp3';
+
+let audiotom1 = new Audio();
+audiotom1.src = 'AudioDrum/tom1.mp3';
+
+let audiofloorTom = new Audio();
+audiofloorTom.src = 'AudioDrum/floor-tom.mp3';
+
+let audioride = new Audio();
+audioride.src = 'AudioDrum/ride.mp3';
+
+let audiobass = new Audio();
+audiobass.src = 'AudioDrum/bass.mp3';
+
+let audiotom2 = new Audio();
+audiotom2.src = 'AudioDrum/tom2.mp3';
+
+let audiohihatFoot = new Audio();
+audiohihatFoot.src = 'AudioDrum/hihat-foot.mp3';
